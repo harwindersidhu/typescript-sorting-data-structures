5785a2809afa5b3d858b5cc871489d4237da6792
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2023-01-15 00:12:58 -0700

    Implemented sorting a string

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection("Xaayb");
+const characterSorter = new Sorter_1.Sorter(charactersCollection);
+characterSorter.sort();
+console.log(charactersCollection.data);
